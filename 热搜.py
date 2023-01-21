217ecb3311991306bfebb64da7a5d88aa7d653c3
Hz46100 <99510072+Hz46100@users.noreply.github.com>
2023-01-21 16:07:02 +0800

    修改一下

--- 热搜.py
+++ 热搜.py
@@ -47,13 +47,13 @@ data = list
 app = Flask(__name__)  # 创建一个服务，赋值给APP
 
 
-@app.route('/get_json', methods=['get'])  # 指定接口访问的路径，支持什么请求方式get，post
-# 讲的是封装成一种静态的接口，无任何参数传入
-def get_user():  # -----这里的函数名称可以任意取
+@app.route('/xxx', methods=['get'])  # 指定接口访问的路径，支持什么请求方式get，post
+# 封装成一种静态的接口，无任何参数传入
+def get_user():  # 这里的函数名称可以任意取
     return jsonify(data)  # 把字典转成json串返回
 
 
-app.run(host='0.0.0.0', port=9669, debug=True)
+app.run(host='0.0.0.0', port=xxx, debug=True)
 # 这个host：windows就一个网卡，可以不写，而liux有多个网卡，写成0:0:0可以接受任意网卡信息,
 # 通过访问127.0.0.1:9669/get_json，可返回data信息
 # debug:调试的时候，可以指定debug=true；如果是提供接口给他人使用的时候，debug要去掉
