6eeac22220a526fe70431f591a6e8674a27114fa
Hz46100 <99510072+Hz46100@users.noreply.github.com>
2023-01-21 16:08:23 +0800

    修改一下

--- 热搜.py
+++ 热搜.py
@@ -48,8 +48,8 @@ app = Flask(__name__)  # 创建一个服务，赋值给APP
 
 
 @app.route('/xxx', methods=['get'])  # 指定接口访问的路径，支持什么请求方式get，post
-# 封装成一种静态的接口，无任何参数传入
-def get_user():  # 这里的函数名称可以任意取
+# 讲的是封装成一种静态的接口，无任何参数传入
+def get_user():  # -----这里的函数名称可以任意取
     return jsonify(data)  # 把字典转成json串返回
 
 
